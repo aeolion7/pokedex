42df0b17ae7a2b1ad30d4316549ef16b915ba986
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-28 08:33:48 -0500

    added centralized express routing

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 4000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next ) => {
+  res.status(200).sendFile(path.join(__dirname, '/public', '/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on port 4000');
+});
