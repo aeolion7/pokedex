32707aabc99c58f4ca32ee3ad3ebf289ac1fc49d
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-29 09:31:01 -0500

    integrated express

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res, next) => {
-  res.status(200).sendFile(path.join(__dirname, '/public', '/index.html'));
+  res.status(200).send('/index');
 });
 
 app.listen(PORT, () => {
