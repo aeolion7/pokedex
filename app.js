5b7d1efeb69f88f5f6899144c99dc5b31e22329e
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-04 09:30:15 -0500

    project restructure

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res, next) => {
-  res.status(200).send('/index');
+  res.status(200).sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 app.listen(PORT, () => {
